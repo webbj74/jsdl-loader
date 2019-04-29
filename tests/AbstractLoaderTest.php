e99a58065826d0686d82a7428003f51cc0c292bb
J Webb <webbj74@users.noreply.github.com>
2019-04-29 14:42:25 -0400

    Skeleton test file for AbstractLoader class.

---
+++ tests/AbstractLoaderTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Webbj74\JSDL\Loader\Tests;
+
+use Webbj74\JSDL\Loader\AbstractLoader;
+
+class AbstractLoaderTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testRemoveAlias()
+    {
+
+    }
+
+    public function testLoad()
+    {
+
+    }
+
+    public function testAddAlias()
+    {
+
+    }
+}
