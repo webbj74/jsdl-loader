9795506bd7ba98b41536f83aa161040492bff869
J Webb <webbj74@users.noreply.github.com>
2019-04-29 14:43:04 -0400

    Fix namespace

--- tests/AbstractLoaderTest.php
+++ tests/AbstractLoaderTest.php
@@ -2,9 +2,10 @@
 
 namespace Webbj74\JSDL\Loader\Tests;
 
+use PHPUnit\Framework\TestCase;
 use Webbj74\JSDL\Loader\AbstractLoader;
 
-class AbstractLoaderTest extends \PHPUnit_Framework_TestCase
+class AbstractLoaderTest extends TestCase
 {
 
     public function testRemoveAlias()
