fbfdd4befe718a50ddeedb6a1144530c25111fdf
J Webb <webbj74@users.noreply.github.com>
2019-04-29 14:51:45 -0400

    Sketch test methods.

--- tests/AbstractLoaderTest.php
+++ tests/AbstractLoaderTest.php
@@ -5,21 +5,38 @@ namespace Webbj74\JSDL\Loader\Tests;
 use PHPUnit\Framework\TestCase;
 use Webbj74\JSDL\Loader\AbstractLoader;
 
+/**
+ * @coversDefaultClass \Webbj74\JSDL\Loader\AbstractLoader
+ */
 class AbstractLoaderTest extends TestCase
 {
 
-    public function testRemoveAlias()
-    {
-
-    }
-
+    /**
+     * @covers ::load
+     */
     public function testLoad()
     {
-
+        $loader = $this->getMockClass(AbstractLoader::class);
+        $result = $loader->load();
     }
 
+    /**
+     * @covers ::addAlias
+     */
     public function testAddAlias()
     {
+        $loader = $this->getMockClass(AbstractLoader::class);
+        $result = $loader->addAlias();
+    }
 
+    /**
+     * @covers ::removeAlias
+     * @depends testAddAlias
+     */
+    public function testRemoveAlias()
+    {
+        $loader = $this->getMockClass(AbstractLoader::class);
+        $result = $loader->addAlias();
+        $result = $loader->removeAlias();
     }
 }
