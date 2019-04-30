3b6e9b8263fa853e1cdb7670796224fd722910ea
J Webb <webbj74@users.noreply.github.com>
2019-04-30 14:02:21 -0400

    Flesh out some tests.

--- tests/AbstractLoaderTest.php
+++ tests/AbstractLoaderTest.php
@@ -10,14 +10,65 @@ use Webbj74\JSDL\Loader\AbstractLoader;
  */
 class AbstractLoaderTest extends TestCase
 {
+    private function getFixturePath($filename)
+    {
+        return __DIR__ . '/fixtures/' . $filename;
+    }
 
     /**
      * @covers ::load
      */
-    public function testLoad()
+    public function testLoadThrowsExceptionIfFileDoesNotExist()
     {
-        $loader = $this->getMockClass(AbstractLoader::class);
-        $result = $loader->load();
+        $loader = $this->getMockForAbstractClass(AbstractLoader::class);
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessageRegExp('/No such file or directory/');
+        $loader->load($this->getFixturePath('nonexistent.json'));
+    }
+
+    /**
+     * @covers ::load
+     */
+    public function testLoadThrowsExceptionIfFileHasUnknownExtension()
+    {
+        $loader = $this->getMockForAbstractClass(AbstractLoader::class);
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessageRegExp('/Unknown file extension/');
+        $loader->load($this->getFixturePath('bad_extension.notjson'));
+    }
+
+    /**
+     * @covers ::load
+     */
+    public function testLoadFromFile()
+    {
+        $loader = $this->getMockForAbstractClass(AbstractLoader::class);
+        $this->confirmValidLoaderFile($loader, $this->getFixturePath('AbstractLoaderTest_testLoadFromFile.json'));
+    }
+
+    /**
+     * @covers ::load
+     * @dataProvider badConfigProvider
+     */
+    public function testLoadThrowsExceptionForBadConfigArgument($config)
+    {
+        $loader = $this->getMockForAbstractClass(AbstractLoader::class);
+        $this->expectException(\InvalidArgumentException::class);
+        $loader->load($config);
+    }
+
+    /**
+     * Data provider for invalid config arguments.
+     *
+     * @return array
+     *   Series of bag arguments to provide.
+     */
+    public function badConfigProvider()
+    {
+        return [
+            'integer'  => [0],
+            'object'  => [(object)['foo' => 'bar']],
+        ];
     }
 
     /**
@@ -25,8 +76,9 @@ class AbstractLoaderTest extends TestCase
      */
     public function testAddAlias()
     {
-        $loader = $this->getMockClass(AbstractLoader::class);
-        $result = $loader->addAlias();
+        $loader = $this->getMockForAbstractClass(AbstractLoader::class);
+        $loader->addAlias('testAddAlias', $this->getFixturePath('AbstractLoaderTest_testAddAlias.json'));
+        $this->confirmValidLoaderFile($loader, 'testAddAlias');
     }
 
     /**
@@ -35,8 +87,38 @@ class AbstractLoaderTest extends TestCase
      */
     public function testRemoveAlias()
     {
-        $loader = $this->getMockClass(AbstractLoader::class);
-        $result = $loader->addAlias();
-        $result = $loader->removeAlias();
+        $loader = $this->getMockForAbstractClass(AbstractLoader::class);
+        $loader->addAlias('testRemoveAlias', $this->getFixturePath('AbstractLoaderTest_testRemoveAlias.json'));
+        $loader->removeAlias('testRemoveAlias');
+        $this->confirmInvalidLoaderFile($loader, 'testRemoveAlias');
+    }
+
+    /**
+     * Confirm ::load will throw exception if invalid filename supplied.
+     *
+     * @param Webbj74\JSDL\Loader\AbstractLoader $loader
+     *   The loader instance to test.
+     * @param string $filename
+     *   The filename to attempt to load.
+     */
+    private function confirmInvalidLoaderFile($loader, $filename)
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessageRegExp('/(No such file or directory|Unknown file extension)/');
+        $loader->load($filename);
+    }
+
+    /**
+     * Confirm ::load will not throw exception if valid filename supplied.
+     *
+     * @param Webbj74\JSDL\Loader\AbstractLoader $loader
+     *   The loader instance to test.
+     * @param string $filename
+     *   The filename to attempt to load.
+     */
+    private function confirmValidLoaderFile($loader, $filename)
+    {
+        // Assumes the best if no exceptions are thrown.
+        $this->assertNull($loader->load($filename));
     }
 }
